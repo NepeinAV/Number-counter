6ac9af2e1b594c8d9f8eafa8af6054c6556184bc
NepeinAV <nepein96@gmail.com>
2017-11-14 23:18:37 +0300

    Optimized js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 'use strict';
-var cvel = document.getElementById("canvas");
-var cv = cvel.getContext("2d");
-var size = 4;
-var h = cvel.offsetHeight;
-var scale = 860 / (size * 2 + 1);
-var count = 1;
-var angle;
 
-cv.translate(450, h / 2);
+var cvel = document.getElementById("canvas"); //Canvas element
+var cv = cvel.getContext("2d"); //2d context
+var size = 7; //размерность
+var h = cvel.offsetHeight;
+var w = cvel.offsetWidth - 40;
+var scale = w / (size * 2 + 1);
+
+cv.translate((w + 40) / 2, h / 2);
 
 function drawXAxis() {
     let s;
-    cv.lineWidth = 1;
+    cv.lineWidth = 2;
     cv.strokeStyle = "#000";
     cv.fillStyle = "#000";
-    cv.font = "16px Calibri";
+    cv.font = "16px Calibri Light";
     cv.textAlign = "center";
     cv.beginPath();
     //ось
-    cv.moveTo(-430, 0);
-    cv.lineTo(430, 0);
+    cv.moveTo(-w / 2, 0);
+    cv.lineTo(w / 2, 0);
     cv.stroke();
     //уголок
-    cv.moveTo(430, -5);
-    cv.lineTo(430, 5);
-    cv.lineTo(435, 0);
+    cv.moveTo(w / 2, -5);
+    cv.lineTo(w / 2, 5);
+    cv.lineTo(w / 2 + 5, 0);
     cv.fill();
     //координаты
     for (var i = 0; i <= size; i++) {
         s = i * scale;
         cv.beginPath();
-        cv.moveTo(s, -5);
-        cv.lineTo(s, 5);
+        cv.moveTo(s, -3);
+        cv.lineTo(s, 3);
         cv.fillText(i, s, -11);
         if (i != 0) {
-            cv.moveTo(-s, -5);
-            cv.lineTo(-s, 5);
+            cv.moveTo(-s, -3);
+            cv.lineTo(-s, 3);
             cv.fillText(-i, -s, -11);
         }
         cv.stroke();
@@ -50,23 +50,19 @@ function arc() {
         x: 0,
         len: 0
     }
+    var count = 1,
+        step = 180 / 20,
+        angle = 0,
+        animReady = -1;
 
     cv.strokeStyle = "#999";
-    cv.lineWidth = 2;
-    cv.font = "16px Calibri";
+    cv.lineWidth = 3;
+    cv.font = "16px Calibri Light";
     cv.fillStyle = "white";
-    drawCirWN();
-    // cv.save();
-    // cv.beginPath();
-    // cv.fillStyle = "#999";
-    // cv.arc(0, 18, 10, 0, 2 * Math.PI, true);
-    // cv.fill();
-    // cv.closePath();
-    // cv.restore();
-
-    // cv.fillText(count, 0, 23);
+    drawCirWN(); //отрисовываем первый номер
 
     this.animateArc = function () {
+        //выходим, если угол становится больше 180 градусов
         if (angle > 180) {
             count++;
             drawCirWN();
@@ -75,33 +71,44 @@ function arc() {
             animReady = true;
             return 1;
         }
+        //скрываем предыдущую дугу
         cv.beginPath();
-        if (count % 2 == 0) {
+        cv.save();
+        cv.strokeStyle = 'white';
+        if (count % 2 == 0)
             cv.arc(prev.x - Math.abs(curr.len), 39, Math.abs(curr.len), (Math.PI / 180) * angle, 0, true);
-            console.log("1", angle);
-        } else {
+        else
             cv.arc(prev.x + Math.abs(curr.len), -29, Math.abs(curr.len), -Math.PI, -(Math.PI - (Math.PI / 180) * angle), false);
-            console.log("-1", angle);
-        }
-        angle += 180 / 30;
         cv.stroke();
-        cv.closePath();
+        cv.restore();
+
+        //рисуем новую дугу
+        cv.beginPath();
+        if (count % 2 == 0)
+            cv.arc(prev.x - Math.abs(curr.len), 39, Math.abs(curr.len), (Math.PI / 180) * angle, 0, true);
+        else
+            cv.arc(prev.x + Math.abs(curr.len), -29, Math.abs(curr.len), -Math.PI, -(Math.PI - (Math.PI / 180) * angle), false);
+        cv.stroke();
+
+        angle += step;
+
         window.requestAnimationFrame(Arc.animateArc);
     }
 
     function drawCirWN() {
         cv.save();
         cv.beginPath();
-        cv.fillStyle = "#999";
+        cv.fillStyle = "#507299";
         cv.arc(curr.x, 21, 12, 0, 2 * Math.PI, true);
         cv.fill();
-        cv.closePath();
         cv.restore();
         cv.fillText(count, curr.x, 26);
     }
 
     this.Next = function () {
         if (count >= size * 2 + 1) return 1;
+        if (!animReady) return 1;
+        animReady = false;
         let x, len;
         if (count % 2 == 0) {
             x = -count / 2 * scale;
@@ -113,20 +120,14 @@ function arc() {
         curr.x = x;
         curr.len = len;
 
-        angle = 0;
         this.animateArc();
     }
 }
 
 function render() {
-    //cv.fillStyle = 'white';
-    //cv.fillRect(-450, -h / 2, 900, h);
-    //drawXAxis();
     Arc.Next();
 }
 
-//cv.setLineDash([100, 100]);
-//cv.lineDashOffset = 90;
 drawXAxis();
 var Arc = new arc();
 
